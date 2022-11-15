0ff98f533332eb0fade232a643ccb17c6a346b14
hw commit <demaniks@yahoo.com>
2022-11-15 22:09:24 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class Main {
+
+    public static int[] generateRandomArray() {
+
+        Random random = new Random();
+        int[] arr = new int[30];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         //task1
+        int totalOfAllPayments = 0;
+        int[] arr = generateRandomArray();
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            //System.out.println(i + " " + arr[i]);
+            totalOfAllPayments = totalOfAllPayments + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + totalOfAllPayments + " рублей.");
     }
 }
