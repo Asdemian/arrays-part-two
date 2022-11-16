8fe9e873dfa8123b7cae9da258904cb16b028cef
hw commit <demaniks@yahoo.com>
2022-11-16 22:03:50 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Random;
 
 public class Main {
 
-    public static int[] generateRandomArray() {
+    public static double[] generateRandomArray() {
 
         Random random = new Random();
-        int[] arr = new int[30];
+        double[] arr = new double[30];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
@@ -15,8 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
         //task1
-        int totalOfAllPayments = 0;
-        int[] arr = generateRandomArray();
+        double totalOfAllPayments = 0;
+        double[] arr = generateRandomArray();
 
         for (int i = 0; i <= arr.length - 1; i++) {
             //System.out.println(i + " " + arr[i]);
@@ -24,8 +24,8 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + totalOfAllPayments + " рублей. ");
         //task2
-        int maxWaste = 1_00_000;
-        int minWaste = 2_00_000;
+        double maxWaste = 1_00_000;
+        double minWaste = 2_00_000;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < minWaste) {
                 //System.out.println(i);
@@ -40,5 +40,15 @@ public class Main {
             }
         }
         System.out.println("Максимальная сумма трат за день составила " +maxWaste+ " рублей");
+
+        //task3
+        double a = arr.length;
+        double b = 0;
+        //System.out.println(a);
+        for (int c = 0; c <= arr.length - 1; c++) {
+            b = b + arr[c];
+        }
+        double averageAmount = b / a;
+        System.out.println("Средняя сумма трат за месяц составила " +averageAmount+ " рублей");
     }
 }
